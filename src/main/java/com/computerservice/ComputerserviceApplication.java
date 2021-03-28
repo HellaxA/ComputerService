b40f6873a1d2c0f63edf7064b2945703f59e9fdb
HellaxA <abereznikov64@gmail.com>
2021-03-28 19:06:27 +0300

    Initial commit

---
+++ src/main/java/com/computerservice/ComputerserviceApplication.java
@@ -0,0 +1,11 @@
+package com.computerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerserviceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerserviceApplication.class, args);
+	}
+}
